ae6f57d07d55f623902ae68255298b078e97485b
evilmurries <cpiwarski00@gmail.com>
2020-08-20 07:36:07 -0700

    intial config and migration

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^(v$vsrs7&1!#@j_%3cwnigcqp*b&nvbfqu$y6htm8f@2!6cnu'
+SECRET_KEY = PROD_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
